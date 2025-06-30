f524537e365d66b7d359f659b9a54ff2e53c5dcc
Smit Vadhel <vadhelsmit@gmail.com>
2025-06-30 11:50:18 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-center bg-neutral-600/35 text-neutral-400 py-2">
+      <div className="flex items-center justify-center gap-4">
+        <Link to="/">About</Link>
+        <Link to="/">Contact</Link>
+      </div>
+      <p className="text-sm">© 2025 Movieo — Built with ❤️ using the TMDb API for movie lovers.</p>
+    </footer>
   )
 }
 
